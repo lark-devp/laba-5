da1446cd15349672ff658601ef1304191cf98674
lark-devp <botrazumniy@gmail.com>
2024-10-05 22:17:04 +0300

    calculate

--- projects/calculator/main.go
+++ projects/calculator/main.go
@@ -1,7 +1,33 @@
 package main
 
-// реализовать calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+import "fmt"
+
+func calculator(firstChan, secondChan <-chan int, stopChan <-chan struct{}) <-chan int {
+	output := make(chan int)
+	go func(ch chan int) {
+		defer close(ch)
+		select {
+		case x := <-firstChan:
+			ch <- x * x
+			fmt.Println("Первый канал")
+		case x := <-secondChan:
+			ch <- x * 3
+			fmt.Println("Второй канал")
+		case <-stopChan:
+			fmt.Println("Стоп")
+
+		}
+
+	}(output)
+	return output
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	stop := make(chan struct{})
+	result := calculator(ch1, ch2, stop)
+	ch1 <- 5
+
+	fmt.Println(<-result)
 }
