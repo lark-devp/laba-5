634b38b5d728bcbf70fa6c3362344d43a89c8f38
lark-devp <botrazumniy@gmail.com>
2024-10-06 12:52:32 +0300

    pipelineIsDone

--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,36 @@
 package main
 
 // реализовать removeDuplicates(in, out chan string)
+import "fmt"
+
+func removeDuplicates(in chan string, out chan string) {
+	var stp string
+	defer close(out)
+	for st := range in {
+		if st != stp {
+			out <- st
+			stp = st
+		}
+	}
+}
 
 func main() {
 	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+	inputStream := make(chan string)
+	outputStream := make(chan string)
+	go removeDuplicates(inputStream, outputStream)
+	var str string
+	fmt.Print("Text has been captured: ")
+	fmt.Scanln(&str)
+	go func() {
+		defer close(inputStream)
+		for _, st := range str {
+			inputStream <- string(st)
+		}
+	}()
+	fmt.Print("Result: ")
+	for st := range outputStream {
+		fmt.Print(st)
+	}
+	fmt.Println("\n", "End")
 }
